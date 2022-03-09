60b1e5dbaff37d731e82cda16fd777971ea7e891
caiobrunholi <caio.brunholi@hotmail.com>
2022-03-09 10:59:07 -0300

    aa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        // Declara e Instancia um objeto Caneta
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIC";
+        c1.cor = "Azul";
+        c1.carga = 100;
+        c1.ponta = 1.0;
+        System.out.println("Minha Caneta:"+c1.modelo);
+        System.out.println("Minha Caneta:"+c1.cor);
+        System.out.println("Minha Caneta:"+c1.carga);
+        System.out.println("Minha Caneta:"+c1.ponta);
     }
 }
--- src/Caneta.java
+++ src/Caneta.java
@@ -1,3 +1,13 @@
 public class Caneta {
-    
+    //caracteristicas da caneta - seus atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Compotamentos das canetas - Seus Métodos
+    void escrever(String texto){
+        System.out.println (texto);
+    } 
+
 }
